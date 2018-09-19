eaadf380cf5937a854a4f54c66692363f9c59fdf
Luke Brierley <ldbrierley@gmail.com>
2018-09-19 16:01:37 +0100

    keep track of Attempts

--- index.py
+++ index.py
@@ -13,9 +13,11 @@ the_random_number = random.randint(1,500)
 
 print("Welcome to the number guessing game.")
 print("Guess a number between 1 and 500 and I will tell you if it was to small or two big")
-
+number_of_guesses = 0
 while True:
     guess = get_guess()
+    number_of_guesses += 1
+    print("You have done " + str(number_of_guesses) + " Attempts")
     if guess == the_random_number:
         print("correct")
         break
